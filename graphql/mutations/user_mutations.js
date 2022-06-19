3d10cf231a38f78c00471f6116a65085bf24ce7e
Habid B Espinosa <hab.bad@hotmail.com>
2022-06-19 20:58:03 +0200

    verifyUser mutation

--- graphql/mutations/user_mutations.js
+++ graphql/mutations/user_mutations.js
@@ -174,3 +174,22 @@ export const updateUser = {
     };
   },
 };
+
+export const verifyUser = {
+  type: UserType,
+  description: "Verify if there is a logged user",
+  resolve: async (_, __, { user }) => {
+    if (!user) throw new Error(errorName.UNAUTHENTICATED);
+    const userTasks = await Task.find({ owner: user._id });
+    const { _id, picture, username, email, createdAt, updatedAt } = user;
+    return {
+      id: _id,
+      picture: picture.secure_url ? picture.secure_url : "",
+      username,
+      email,
+      createdAt,
+      updatedAt,
+      userTasks,
+    };
+  },
+};
